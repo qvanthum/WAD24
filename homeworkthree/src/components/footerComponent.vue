75ff77a5dc1f0b9a9387d3c803278820fbf926e3
Elerin Lõhmus <elerin.lohmus@ut.ee>
2024-11-25 20:52:20 +0200

    Added footer

--- homeworkthree/src/components/footerComponent.vue
+++ homeworkthree/src/components/footerComponent.vue
@@ -1,11 +1,38 @@
 <template>
-<p>Test</p>
+  <footer>
+    <div class="footer-container">
+      <p id="footer-text">Like your favourite post!</p>
+    </div>
+  </footer>
 </template>
-
 <script>
-
+export default {
+  name: 'footerComponent',
+}
 </script>
+<style scoped>
+#footer-text {
+  font-size: 40px;
+  text-align: center;
+  margin: 0;
+}
 
-<style>
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #BFBFBF;
+  height: 150px;
+  margin: 20px 40px;
+  padding: 10px;
+  border-radius: 25px;
+}
 
+.footer-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
 </style>
