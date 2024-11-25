da088cd5770166ebfaae3d4d842b8f0b9715a6cb
qvanthum <evertsaarnak@gmail.com>
2024-11-25 16:57:50 +0200

    Created template files and some structure

--- homeworkthree/src/App.vue
+++ homeworkthree/src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <headerComponent/>
+    <sidebarLeft/>
+    <sidebarRight/>
+    <footerComponent/>
+  </div>
 </template>
+
+<script>
+import headerComponent from './components/headerComponent.vue';
+import sidebarLeft from './components/sidebarLeft.vue';
+import sidebarRight from './components/sidebarRight.vue';
+import footerComponent from './components/footerComponent.vue';
+
+</script>
 
 <style>
 #app {
