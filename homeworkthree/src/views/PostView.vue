811a009a122fbbd7b2b9100a87716f8df32fa5cc
Elerin Lõhmus <elerin.lohmus@ut.ee>
2024-12-17 23:13:53 +0200

    added styling for post view

--- homeworkthree/src/views/PostView.vue
+++ homeworkthree/src/views/PostView.vue
@@ -1,19 +1,29 @@
 <template>
     <div class="middle">
-        <postComponent v-if="post" :post="post" @increment="incrementLikes" />
-        <p v-else>Post not found</p>
+        <!-- <postComponent v-if="post" :post="post" @increment="incrementLikes" />
+        <p v-else>Post not found</p> -->
+        <form @submit.prevent="">
+            <div>
+                <p>A Post</p>
+            </div>
+            <div class="form-body">
+                <label for="body">Body</label>
+                <textarea name="body" id="" :placeholder="post.postText"></textarea>
+            </div>
+            <div class="action-button-group">
+                <button class="action-button">Update</button>
+                <button class="action-button">Delete</button>
+            </div>
+        </form>
     </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from "vuex";
-import postComponent from "@/components/postComponent.vue";
 
 export default {
     name: "postView",
-    components: {
-        postComponent,
-    },
+    components: {},
     computed: {
         ...mapState(["posts"]),
         ...mapGetters(["posts"]),
@@ -37,4 +47,37 @@ export default {
   margin: 20px;
   order: 2;
 }
+
+form {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+}
+
+.form-body {
+    display: flex;
+    justify-content: space-between;
+    gap: 80px;
+}
+
+.action-button-group {
+    display: flex;
+    justify-content: space-around;
+    gap: 35px;
+}
+
+.action-button {
+  width: 100px;
+  padding: 10px;
+  background-color: crimson;
+  color: white;
+  border: none;
+  border-radius: 25px;
+  cursor: pointer;
+}
+
+.action-button:hover {
+  opacity: 0.8;
+}
+
 </style>
